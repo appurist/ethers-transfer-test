f9c903f3b46a5b69fee106dd8ffe7d3b8c5ec4aa
Paul Whittemore <dev@appurist.com>
2022-12-03 23:55:46 -0400

    Synchronized a bit with the claimReward code and it's working.
    Wrong contract ID in the .env file all this time. #fail

--- main.mjs
+++ main.mjs
@@ -13,8 +13,8 @@ function getConfigValue(key) {
   return process.env[key];
 }
 const receiver = getConfigValue("RECEIVER_ADDRESS") || "0x83Ac670f03bAe84e7A0275C32f1B75F59aCDa879";
-const amount = ethers.utils.parseEther(getConfigValue("AMOUNT") || "1");
-const deskAmount = ethers.utils.formatEther(amount);
+const amountWei = ethers.utils.parseEther(getConfigValue("AMOUNT") || "1");
+const amountDesk = ethers.utils.formatEther(amountWei);
 
 let provider;
 let signer;
@@ -29,43 +29,43 @@ try {
   process.exit(1);
 }
 
-console.log(`Sending ${amount} (${deskAmount} DESK) to ${receiver}`);
+console.log(`Sending ${amountWei} (${amountDesk} DESK) to ${receiver}`);
 
 try {
   // construct transaction
   const desk = await contract.connect(signer);
   const txnProps = {
     gasLimit: 100000,
-    gasPrice: 2000000000,
+    gasPrice: 100000000000, //ethers.utils.parseUnits("100", "gwei"),
   };
-  const draftTxn = await desk.populateTransaction.transfer(receiver, amount, txnProps);
-  console.log("Draft transaction: ",draftTxn);
-  console.log("gasLimit: ",draftTxn.gasLimit);
-  console.log("gasPrice (gwei): ",ethers.utils.formatUnits(draftTxn.gasPrice, "gwei"));
+  const draftTxn = await desk.populateTransaction.transfer(receiver, amountWei, txnProps);
+  console.log("Draft transaction:",draftTxn);
+  console.log("gasLimit: ",draftTxn.gasLimit.toString());
+  console.log(`gasPrice: ${ethers.utils.formatUnits(draftTxn.gasPrice, "gwei")} gwei`);
   // gas estimate
-  const estimate = await desk.estimateGas.transfer(receiver, amount, txnProps);
+  const estimate = await desk.estimateGas.transfer(receiver, amountWei, txnProps);
   console.log(`Estimate: ${estimate} GAS`);
   // dry-run
   console.log('Attempting dry-run.');
-  const dryRun = await desk.callStatic.transfer(receiver, amount, txnProps);
-  console.log(dryRun);
+  const dryRun = await desk.callStatic.transfer(receiver, amountWei, txnProps);
+  console.log("Dry run complete:", dryRun);
   // transfer
-  console.log(`Sending ${amount} DESK (${amount} wei) from ${signer.address} to ${receiver}...`);
-  const receipt = await desk.transfer(receiver, amount, txnProps);
+  console.log(`Sending ${amountWei} wei (${amountDesk} DESK) from ${signer.address} to ${receiver}...`);
+  const receipt = await desk.transfer(receiver, amountWei, txnProps);
   if (!receipt) {
-    console.log(`Transfer of ${amount} (${deskAmount} DESK) to ${receiver}, NO RECEIPT!`);
-    throw new Error(`claimReward: Transfer of ${amount} (${deskAmount} DESK) to ${receiver}: could not generate receipt.`);
+    console.log(`Transfer of ${amountWei} (${amountDesk} DESK) to ${receiver}, NO RECEIPT!`);
+    throw new Error(`claimReward: Transfer of ${amountWei} (${amountDesk} DESK) to ${receiver}: could not generate receipt.`);
   }
-  console.log(`Sent ${amount} (${deskAmount} DESK) from ${signer.address} to ${receiver}.`);
+  console.log(`Sent ${amountWei} (${amountDesk} DESK) from ${signer.address} to ${receiver}.`);
   try {
     console.log(JSON.stringify(receipt, null, 2));
-    console.log(`Transfer of ${amount} (${deskAmount} DESK), receipt OK: ${receipt.hash}`);
+    console.log(`Transfer of ${amountWei} (${amountDesk} DESK), receipt OK: ${receipt.hash}`);
   } catch (e) {
     console.log(e.message);
   }
 } catch (err) {
   let message = err?.error?.error?.message || err?.error?.reason || err?.reason || "";
-  console.log(`Error sending ${amount} (${deskAmount} DESK) to ${receiver}:`, message);
+  console.log(`Error sending ${amountWei} (${amountDesk} DESK) to ${receiver}:`, message);
   if (err?.message) {
     console.log(err.message);
   }
