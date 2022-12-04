be6d4211051191851d8765c8adddc974db44e47f
Paul Whittemore <dev@appurist.com>
2022-12-03 21:40:53 -0400

    The test transfer that is always pending. :(

--- main.mjs
+++ main.mjs
@@ -1,19 +1,62 @@
 import { ethers } from "ethers";
 import DESK_CONTRACT_ABI from "./DESK.json" assert { type: "json" };
-
 import {config as dotConfig} from 'dotenv'
 dotConfig()
-
-function getConfigValue(key) {
-    const value = process.env[key];
-    if (value === undefined) {
-        throw new Error(`Missing config value: ${key}`);
-    }
-    return value;
-}
 
 const PROVIDER_NETWORK = getConfigValue("PROVIDER_NETWORK");
 const ALCHEMY_KEY = getConfigValue("ALCHEMY_KEY");
 
 const DESK_CONTRACT_ADDRESS = getConfigValue("DESK_CONTRACT_ADDRESS");
 const SIGNER_PRIVATE_KEY = getConfigValue("SIGNER_PRIVATE_KEY");
+
+function getConfigValue(key) {
+  return process.env[key];
+}
+const receiver = getConfigValue("RECEIVER_ADDRESS") || "0x83Ac670f03bAe84e7A0275C32f1B75F59aCDa879";
+const amount = ethers.utils.parseEther(getConfigValue("AMOUNT") || "1");
+const deskAmount = ethers.utils.formatEther(amount);
+console.log(`Sending ${amount} (${deskAmount} DESK) to ${receiver}`);
+
+let receipt = null;
+let result = null;
+try {
+  const provider = new ethers.providers.AlchemyProvider(PROVIDER_NETWORK, ALCHEMY_KEY);
+  const signer = new ethers.Wallet(SIGNER_PRIVATE_KEY, provider);
+  const contract = new ethers.Contract(DESK_CONTRACT_ADDRESS, DESK_CONTRACT_ABI, signer);
+
+  // construct transaction
+  const desk = await contract.connect(signer);
+  const txnProps = {
+    gasLimit: 1000000, // 10^6 (dimensionless value)
+    gasPrice: 10000000000, // 10^10 wei
+  };
+  const draftTxn = await desk.populateTransaction.transfer(receiver, amount, txnProps);
+  console.log(draftTxn);
+  // gas estimate
+  const estimate = await desk.estimateGas.transfer(receiver, amount, txnProps);
+  console.log(`Estimate: ${estimate} GAS`);
+  // dry-run
+  console.log('Attempting dry-run.');
+  const dryRun = await desk.callStatic.transfer(receiver, amount, txnProps);
+  console.log(dryRun);
+  // transfer
+  console.log(`Sending ${amount} DESK (${amount} wei) from ${signer.address} to ${receiver}...`);
+  const receipt = await desk.transfer(receiver, amount, txnProps);
+  console.log(`Sent ${amount} (${deskAmount} DESK) from ${signer.address} to ${receiver}.`);
+
+  if (!receipt) {
+    console.log(`Transfer of ${amount} (${deskAmount} DESK) to ${receiver}, NO RECEIPT!`);
+    throw new Error(`claimReward: Transfer of ${amount} (${deskAmount} DESK) to ${receiver}: could not generate receipt.`);
+  }
+
+  console.log(`Transfer of ${amount} (${deskAmount} DESK), receipt OK: ${receipt.hash}`);
+  try {
+    console.log(JSON.stringify(receipt, null, 4));
+  } catch (e) {
+    console.log(receipt);
+  }
+  console.log(JSON.stringify(receipt, null, 2));
+} catch (err) {
+  let message = err?.error?.error?.message || err?.error?.reason || err?.reason || err?.message;
+  console.log(`Error sending ${amount} (${deskAmount} DESK) to ${receiver}:`, message);
+}
